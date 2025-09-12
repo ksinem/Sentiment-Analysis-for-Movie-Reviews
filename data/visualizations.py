920689643cda42a583e339cee53ba3764746201e
Sinem <demiraslan>
2025-09-12 12:40:05 +0200

    added helper function for visualizations

--- data/visualizations.py
+++ data/visualizations.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import re
 from ydata_profiling import ProfileReport
+import nltk
+from nltk.corpus import stopwords
+
+
+def clean_and_remove_stopwords(text):
+    text = str(text)
+    text = re.sub(r'<[^>]+>', '', text)
+    stop_words = set(stopwords.words('english'))
+    words = [word for word in text.split() if word.lower() not in stop_words]
+    return " ".join(words)
+
 
 data = pd.read_csv("IMDB Dataset.csv")
-
-data["reviews_clean"] = data["review"].apply(lambda word: re.sub(r'<[^>]+>', '', str(word)))
-
+data["reviews_clean"] = data["review"].apply(clean_and_remove_stopwords)
+# Generate col containing the length of each review
 data['review_length'] = data['reviews_clean'].apply(lambda x: len(x.split()))
-print(data.describe())
-profile = ProfileReport(data, title="IMDB Review Data")
+profile = ProfileReport(data, title="IMDB Review Data", explorative=True)
 profile.to_file("imdb_report.html")
-
 
 
